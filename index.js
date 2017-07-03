b02c0488074caf63cf876704fad7be95702a49d0
mbosek <bosek.mateusz@gmail.com>
2017-07-03 22:58:52 +0200

    Add config to S3 and Polly

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 const AWS = require('aws-sdk')
 const Fs = require('fs')
 const uuid = require('node-uuid');
+
+const s3 = new AWS.S3();
+
+const bucketName = 'audio-bucket-' + uuid.v4();
+const keyName = 'beercoders.mp3';
+
+const Polly = new AWS.Polly({
+    signatureVersion: 'v4',
+    region: 'us-east-1'
+})
+
+const params = {
+    'Text': 'Hi, good to here you!',
+    'OutputFormat': 'mp3',
+    'VoiceId': 'Justin'
+}
